85c6b215761cf580b6a8c5992420d20242a7d2e1
Md Robin Rana <developer.smrobin45@gmail.com>
2023-11-08 22:28:45 +0600

    Pagination added - all assignments page

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,25 +1,39 @@
-const Pagination = ({ totalPage, setPageNumber }) => {
-   const pageNumbers = [];
-   for (let i = 1; i <= totalPage; i++) {
-      pageNumbers.push(i);
-   }
+import { Button, IconButton } from "@material-tailwind/react";
+
+const Pagination = ({
+   currentPage,
+   setCurrentPage,
+   pageNumbers,
+   endIndex,
+   filteredAssignment,
+}) => {
    return (
-      <div>
-         <nav className="pt-20">
-            <ul className="flex">
-               {pageNumbers.map((number, index) => (
-                  <li key={index}>
-                     <button
-                        onClick={() => {
-                           setPageNumber(number);
-                        }}
-                        className="mx-1 flex h-9 w-9 items-center justify-center rounded-full border border-blue-gray-100 bg-transparent p-0 text-sm text-blue-gray-500 transition duration-150 ease-in-out hover:bg-light-300">
-                        <span className="material-icons text-sm">{number}</span>
-                     </button>
-                  </li>
-               ))}
-            </ul>
-         </nav>
+      <div className="flex items-center gap-4 justify-center">
+         <Button
+            variant="text"
+            className="flex items-center gap-2"
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}>
+            Previous
+         </Button>
+         <div className="flex items-center gap-2">
+            {pageNumbers.map((number) => (
+               <>
+                  <IconButton
+                     disabled={number === currentPage}
+                     onClick={() => setCurrentPage(number)}>
+                     {number}
+                  </IconButton>
+               </>
+            ))}
+         </div>
+         <Button
+            variant="text"
+            className="flex items-center gap-2"
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={endIndex >= filteredAssignment.length}>
+            Next
+         </Button>
       </div>
    );
 };
--- src/Containers/AssignmentsContainer/AssignmentArchive.jsx
+++ src/Containers/AssignmentsContainer/AssignmentArchive.jsx
@@ -1,12 +1,15 @@
 import { Option, Select } from "@material-tailwind/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ClipLoader } from "react-spinners";
 import AssignmentCard from "../../Components/AssignmentCard/AssignmentCard";
+import Pagination from "../../Components/Pagination/Pagination";
 import useAssignments from "../../Hooks/useAssignments";
 
 const AssignmentArchive = () => {
    const { assignments, isPending } = useAssignments();
    const [filteredAssignment, setFilteredAssignment] = useState([]);
+   const [currentPage, setCurrentPage] = useState(1);
+   const itemsPerPage = 6;
 
    const handleFilter = (value) => {
       if (!isPending) {
@@ -18,8 +21,27 @@ const AssignmentArchive = () => {
             );
             setFilteredAssignment(filtered);
          }
+         setCurrentPage(1);
       }
    };
+
+   const startIndex = (currentPage - 1) * itemsPerPage;
+   const endIndex = startIndex + itemsPerPage;
+   const pageAssignments = filteredAssignment.slice(startIndex, endIndex);
+
+   const totalPages = Math.ceil(filteredAssignment.length / itemsPerPage);
+
+   const pageNumbers = [];
+   for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+   }
+
+   useEffect(() => {
+      if (!isPending && filteredAssignment.length === 0) {
+         const defaultAssignments = assignments.slice(0, itemsPerPage);
+         setFilteredAssignment(defaultAssignments);
+      }
+   }, [isPending, assignments, filteredAssignment]);
    return (
       <>
          <div className="mt-20 mb-10 flex flex-col items-end">
@@ -35,26 +57,20 @@ const AssignmentArchive = () => {
          {!isPending ? (
             <>
                <div className="grid mb-20 grid-cols-1 md:grid-cols-3 gap-y-10 gap-x-4">
-                  {!filteredAssignment.length ? (
-                     <>
-                        {assignments.map((assignment) => (
-                           <AssignmentCard
-                              assignmentData={assignment}
-                              key={assignment._id}
-                           />
-                        ))}
-                     </>
-                  ) : (
-                     <>
-                        {filteredAssignment.map((assignment) => (
-                           <AssignmentCard
-                              assignmentData={assignment}
-                              key={assignment._id}
-                           />
-                        ))}
-                     </>
-                  )}
+                  {pageAssignments.map((assignment) => (
+                     <AssignmentCard
+                        assignmentData={assignment}
+                        key={assignment._id}
+                     />
+                  ))}
                </div>
+               <Pagination
+                  setCurrentPage={setCurrentPage}
+                  currentPage={currentPage}
+                  pageNumbers={pageNumbers}
+                  endIndex={endIndex}
+                  filteredAssignment={filteredAssignment}
+               />
             </>
          ) : (
             <div className="h-screen flex flex-col items-center justify-center">
